1aeb671c4d8b90ad0bc6025a4c29a6dc1f0d408e
Mochaharpy <166887671+Mochaharpy@users.noreply.github.com>
2025-09-19 12:39:52 -0700

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = 'my-offline-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/console.min.js',
+  '/core.min.js',
+  '/editor.min.js',
+  '/file-actions.min.js',
+  '/importJSON.min.js',
+  '/preview.min.js',
+  '/resize.min.js',
+  '/zip.min.js',
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
